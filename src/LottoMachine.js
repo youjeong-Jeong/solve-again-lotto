205fe74f78b770fe22633ecf6c7b4e5621439a7e
youjeong Jeong <wjddbwjd1205@naver.com>
2023-12-14 17:03:22 +0900

    feat: 당첨 번호 입력 받기

--- src/LottoMachine.js
+++ src/LottoMachine.js
@@ -8,12 +8,25 @@ class LottoMachine {
   async start() {
     const money = await this.userInputMoney();
     const winningNumber = await this.userInputWinningNumber();
-    const lottoTickets = 
+    const lottoTickets = this.getTicketsCount(money);
+    
+    console.log(money);
+    console.log(winningNumber);
+    console.log(lottoTickets);
+  }
+
+  getTicketsCount(money = 0) {
+    return money / 1000;
   }
 
   async userInputWinningNumber() {
-    const winningNumber = await InputView.readWinningNumber();
-    this.validateWinningNumber(winningNumber);
+    const winningNumberInput = await InputView.readWinningNumber();
+    this.validateWinningNumber(winningNumberInput);
+    const winningNumber = [];
+    winningNumberInput.split(',').forEach( number => {
+      winningNumber.push(Number(number));
+    })
+    return winningNumber;
   }
 
   validateWinningNumber(winningNumber = '') {
@@ -23,10 +36,19 @@ class LottoMachine {
 
   validateOutOfRange(winningNumber = '') {
     const numbers = winningNumber.split(',');
+    const numberList = [];
     numbers.forEach( number => {
-      this.validateNumber(number);
+      numberList.push(this.validateNumber(number));
     })
-    this.validateDuplicate(numbers);
+    this.validateDuplicate(numberList);
+  }
+
+  validateDuplicate(numberList = '') {
+    const list = [...numberList];
+    const uniqueValues = [...new Set(list)];
+    if (uniqueValues.length !== list.length) {
+      throw new Error(ERROR.number);
+    }
   }
 
   validateNumber(input = '') {
@@ -34,12 +56,13 @@ class LottoMachine {
     if(number < 1 || number > 45) {
       throw new Error(ERROR.number);
     }
+    return number;
   }
 
   async userInputMoney() {
     const money = await InputView.readPrice();
     this.validateMoney(money);
-    return money;
+    return Number(money);
     /*
     while (true) {
       try {
