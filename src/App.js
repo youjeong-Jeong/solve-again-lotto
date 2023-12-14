79a170a294263e36b7bf4f5500dc239d5c49c21e
youjeong Jeong <wjddbwjd1205@naver.com>
2023-12-14 18:49:44 +0900

    feat: 프로젝트 실행을 위해 App.js에 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import LottoMachine from "./LottoMachine.js";
+
 class App {
-  async play() {}
+  async play() {
+    const lottoMachine = new LottoMachine();
+    await lottoMachine.start();      
+  }
 }
 
 export default App;
